a3382eaa7cfdd6250c2d1a5823462024cd67c019
jangsunghyun <mokacoffee1@nate.com>
2019-04-09 10:35:41 +0900

    convertTyep 부동소수점 예제

--- ConvertTypeEx/ConvertTypeEx/Program.cs
+++ ConvertTypeEx/ConvertTypeEx/Program.cs
@@ -16,8 +16,29 @@ namespace ConvertTypeEx
             /*
              데이터 형식 변환(형변환) : 변수를 다른 데이터 형식에 옮기는 것을 의미
 
+                - 크기가 서로 다른 정수형 사이의 형변환
+                - 크기가 서로 다른 부동소수점 사이의 형변환
+                - 부호있는 정수형과 부호없는 정수형 사이의 형변환
+                - 실수형(부동소수점)과 정수형 사이의 형변환
+                - 문자열과 숫자 사이의 형변환
 
              */
+
+            sbyte a = 127;
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(a);
+
+            int aa = (int)a;
+            Console.WriteLine(aa);
+
+            int x = 128;
+            Console.WriteLine(x);
+
+            sbyte xx = (sbyte)x;
+            Console.WriteLine(xx);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ConvertTypeEx2/ConvertTypeEx2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertTypeEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //부동 소수점 형식 사이의 형변환
+            float f = 79.6777f;
+            Console.WriteLine("f : {0}", f);
+
+            Double d = (double)f;
+            Console.WriteLine("d : {0}", d);
+        }
+    }
+}
