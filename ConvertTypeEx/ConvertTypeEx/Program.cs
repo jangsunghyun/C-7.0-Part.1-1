e37ce5e6a8ad21f602d74d62fa6d40fec0338ffb
jangsunghyun <mokacoffee1@nate.com>
2019-04-09 10:01:29 +0900

    데이터 현환변환 1

---
+++ ConvertTypeEx/ConvertTypeEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertTypeEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 데이터 형식 변환하기(Type Conversion)
+            // 소괄호 ( ) 컨버전 연산자
+
+            /*
+             데이터 형식 변환(형변환) : 변수를 다른 데이터 형식에 옮기는 것을 의미
+
+
+             */
+        }
+    }
+}
