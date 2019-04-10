79fdc4f79bb8a683aacb6539160da0a96cf9a3cc
jangsunghyun <mokacoffee1@nate.com>
2019-04-10 09:49:00 +0900

    연산자..

--- OperatorEx/OperatorEx/Program.cs
+++ OperatorEx/OperatorEx/Program.cs
@@ -13,6 +13,23 @@ namespace OperatorEx
             //연산자
             int a = 100 + 200;
             Console.WriteLine(a);
+
+            int mod = 13 % 2;
+            Console.WriteLine(mod);
+
+            int aa = 10;
+
+            Console.WriteLine(aa++);
+            Console.WriteLine(++aa);
+
+            Console.WriteLine(aa--);
+            Console.WriteLine(--aa);
+
+            bool res;
+            res = 10 > 100;
+            res = 100 == 100;
+            res = 11 != 20;
+            Console.WriteLine(res);
         }
     }
 }
