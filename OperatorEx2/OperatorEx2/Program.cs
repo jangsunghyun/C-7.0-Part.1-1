d0ebc6bc49d697e93a8671d0b1b589eae2f03802
jangsunghyun <mokacoffee1@nate.com>
2019-04-10 10:44:50 +0900

    연산자..

---
+++ OperatorEx2/OperatorEx2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //논리연산자
+            Console.WriteLine("----------------AND-----------------------");
+            Console.WriteLine(10 > 5 && 10 < 100);
+            Console.WriteLine(10 > 5 && 10 < 5);
+
+            // !(Not 연산자)
+            Console.WriteLine("----------------Not-----------------------");
+            Console.WriteLine(!true);
+            Console.WriteLine(!false);
+
+            //조건 연산자 ?
+            // 조건식 ? 참일때 값 : 거짓일때 값
+            String str = (100 % 2) == 0 ? "짝수" : "홀수";
+            Console.WriteLine(str);
+
+
+        }
+    }
+}
---
+++ OperatorEx4/OperatorEx4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorEx4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //비트연산자 (쉬프트 , &, |, ^)
+            Console.WriteLine("-----------------쉬프트 연산 ----------------");
+
+            int aa = 1, bb = 2, cc = 3;
+            // 특정형식에 맞춰 출력하는 포맷팅 (Formatting) 기능
+            //Console.WriteLine("aa:{0} bb:{1} cc:{2}", aa, bb, cc);
+
+            //int a = 3;
+            //Console.WriteLine("{0:D5}", a); //10진수 표기
+            int a = 1;            
+            Console.WriteLine("{0:D3} 0x{0:X8}", aa);  //16진수
+            Console.WriteLine("{0:D3} 0x{0:x8}", a << 1); //
+            Console.WriteLine("{0:D3} 0x{0:x8}", a << 5);
+        }
+    }
+}
